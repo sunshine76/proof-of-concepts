0223412c76a04d4461a449d34a44506842d04a3e
schinta6 <surya_chinta@uhc.com>
2016-01-27 22:44:34 -0500

    testing git commit hook

--- reference-app/src/main/java/org/referenceapp/config/ApplicationConfiguration.java
+++ reference-app/src/main/java/org/referenceapp/config/ApplicationConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 /*
-  * Created by surya ch
+  * Created by surya
  */
 @Configuration
 @ComponentScan("org.referenceapp")
